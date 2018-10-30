a8fbe970113a575e900944fb888455e95648bbd1
kneuma <kaylashimbashi@gmail.com>
2018-10-30 17:10:13 -0600

    add list to welcome blade with webroute

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::apiResource('tasks', 'TaskController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
